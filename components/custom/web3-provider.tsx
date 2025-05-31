6e869629b81db9acd4f8d1fd81722b97cb5ae8a2
DigitalDominance <digitaldominanceseo@gmail.com>
2025-05-30 20:23:33 -0400

    Update web3-provider.tsx

--- components/custom/web3-provider.tsx
+++ components/custom/web3-provider.tsx
@@ -2,18 +2,15 @@
 
 import React from "react"
 import { WagmiConfig } from "wagmi"
-import { QueryClientProvider } from "@tanstack/react-query"
-import { queryClient, wagmiConfig } from "@/lib"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { wagmiConfig } from "../../lib/web3modal-config"
 
-export function Web3Provider({
-  children,
-  initialState,
-}: {
-  children: React.ReactNode
-  initialState?: unknown
-}) {
+// Instantiate a single QueryClient for React Query
+const queryClient = new QueryClient()
+
+export function Web3Provider({ children }: { children: React.ReactNode }) {
   return (
-    <WagmiConfig config={wagmiConfig} initialState={initialState}>
+    <WagmiConfig config={wagmiConfig}>
       <QueryClientProvider client={queryClient}>
         {children}
       </QueryClientProvider>
