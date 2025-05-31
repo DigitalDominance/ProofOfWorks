47fb9d0e8dbf7f3b425014780ec9fb58c56c49a6
DigitalDominance <digitaldominanceseo@gmail.com>
2025-05-30 20:20:34 -0400

    Update web3-provider.tsx

--- components/custom/web3-provider.tsx
+++ components/custom/web3-provider.tsx
@@ -1,18 +1,22 @@
 "use client"
 
-import type React from "react"
-// Removed useEffect and initializeWeb3Modal import
+import React from "react"
 import { WagmiConfig } from "wagmi"
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import { wagmiConfig } from "@/lib/web3modal-config"
+import { QueryClientProvider } from "@tanstack/react-query"
+import { queryClient, wagmiConfig } from "@/lib"
 
-const queryClient = new QueryClient()
-
-export function Web3Provider({ children, initialState }: { children: React.ReactNode; initialState?: any }) {
-  // Removed the useEffect for initializeWeb3Modal
+export function Web3Provider({
+  children,
+  initialState,
+}: {
+  children: React.ReactNode
+  initialState?: unknown
+}) {
   return (
     <WagmiConfig config={wagmiConfig} initialState={initialState}>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-    </WagmiProvider>
+      <QueryClientProvider client={queryClient}>
+        {children}
+      </QueryClientProvider>
+    </WagmiConfig>
   )
 }
